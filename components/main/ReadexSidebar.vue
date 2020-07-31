0dd1b35159fc6551a08d570e144fcded51e6005a
Phu Nguyen D <ngdphu.info@gmail.com>
2020-07-31 10:24:54 +0700

    - Update scratch sidebar.

---
+++ components/main/ReadexSidebar.vue
@@ -0,0 +1,51 @@
+<template>
+    <div :class="$style.sidebar">
+        <a-menu
+            style="width: 250px;"
+            :default-selected-keys="['1']"
+            :open-keys.sync="openKeys"
+            mode="inline"
+            @click="handleClick"
+            theme="dark"
+            :class="$style.menu"
+        >
+            <a-menu-item :class="$style.item" key="1">
+                Load book
+            </a-menu-item>
+            <!-- <a-menu-item :class="$style.item" key="2">
+                Option 2
+            </a-menu-item>
+            <a-menu-item :class="$style.item" key="3">
+                Option 3
+            </a-menu-item> -->
+        </a-menu>
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default {
+    data() {
+        return {
+            current: ["mail"],
+            openKeys: ["sub1"],
+        };
+    },
+    watch: {
+        openKeys(val: any) {
+            console.log("openKeys", val);
+        },
+    },
+    methods: {
+        handleClick(e: any) {
+            console.log("click", e);
+        },
+        titleClick(e: any) {
+            console.log("titleClick", e);
+        },
+    },
+};
+</script>
+
+<style module src="./ReadexSidebar.less" lang="less"></style>
---
+++ layouts/dark.vue
@@ -0,0 +1,20 @@
+<template>
+    <div :class="$style.dark">
+        <ReadexSidebar></ReadexSidebar>
+        <Nuxt />
+    </div>
+</template>
+
+<script>
+import Vue from "vue";
+
+import ReadexSidebar from "../components/main/ReadexSidebar.vue";
+
+export default {
+    components: {
+        ReadexSidebar,
+    },
+};
+</script>
+
+<style module src="./dark.less" lang="less"></style>
